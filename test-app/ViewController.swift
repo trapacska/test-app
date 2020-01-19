3a46b3f0225bae1753f445cffa1dc4aad499f3aa
Tamas Papik <tamas.papik@tamas-papik-temp-mbp.local>
2020-01-19 12:52:17 +0100

    init

---
+++ test-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test-app
+//
+//  Created by Tamas Papik on 1/19/20.
+//  Copyright © 2020 Tamas Papik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
